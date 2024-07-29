f41218fe9d993daac52e7f0c06d091a7613816d2
BEN <112600905+ALiENxBEN@users.noreply.github.com>
2024-07-30 03:37:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://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:x-oauth-basic@github.com/ALiENxBEN/Forward-bot-v69 -b $BRANCH /Forward-bot-v69
 fi
-cd JishuDeveloper/Ultra-Forward-Bot
+cd ALiENxBEN/Forward-bot-v69
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
