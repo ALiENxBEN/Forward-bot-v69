bdfd39182f1548d0c9a0136ebe25efac7f836eeb
BEN <112600905+ALiENxBEN@users.noreply.github.com>
2024-07-30 03:24:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,13 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
-
-
-
-import os
+from os import environ 
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
-
+    API_ID = environ.get("API_ID", "9015363")
+    API_HASH = environ.get("API_HASH", "1a6107cae9c5a5f869a0b23cc26ac382")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7245532732:AAEt7wihdB12YHMJWryy4p5VD8K_F6YUuP4") 
+    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://anicade:anicade@anicade.7igizof.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6263157611').split()]
 
 class temp(object): 
     lock = {}
@@ -24,15 +15,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
